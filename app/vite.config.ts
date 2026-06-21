13e156681d0e7243f7d30b61748ef57102ecd7fd
Denis Kostiuk <botoman45354@gmail.com>
2026-06-21 16:32:41 +0300

    Fixe builder configuration

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,24 +1,21 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig(({ mode }) => ({
-    build: {
-        outDir: './../dist',
-        chunkSizeWarningLimit: 1000,
+  build: {
+    outDir: "./../dist",
+    chunkSizeWarningLimit: 1000,
+  },
+  base: mode == "development" ? "" : "./",
+  plugins: [vue(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    base: mode == 'development' ? '' : './',
-    plugins: [
-      vue(),
-      tailwindcss(),
-    ],
-    server: {
-        port: 3000,
-    },
-    resolve:{
-        alias:{
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
+  },
 }));
