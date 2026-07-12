335f0d525eeed53526d264f3c2b34ed1a85c6634
Denis Kostiuk <64838929+Chrumba@users.noreply.github.com>
2026-07-12 21:37:51 +0300

    Implement open page logic

--- app/src/modules/page/components/editor/nodes/blocks/PageLinkBlock.vue
+++ app/src/modules/page/components/editor/nodes/blocks/PageLinkBlock.vue
@@ -18,12 +18,14 @@
 
 <script setup lang="ts">
 import { useGlobalObjectStore } from "@/core/store/globalObjectStore";
+import { useGlobalNavigation } from "@/core/store/navigationStore";
 import type { Icon } from "@/core/types";
 import BaseIcon from "@/shared/components/icon/BaseIcon.vue";
 import DynamicIcon from "@/shared/components/icon/DynamicIcon.vue";
 import { ref, computed, watchEffect, type Ref } from "vue";
 
-const store = useGlobalObjectStore();
+const globalObjectStore = useGlobalObjectStore();
+const globalNavigationStore = useGlobalNavigation();
 
 const props = defineProps<{
   nodeAttributes: Record<string, any>;
@@ -51,7 +53,7 @@ watchEffect(async () => {
   isLoading.value = true;
 
   try {
-    const res = await store.getMetaInfo(id);
+    const res = await globalObjectStore.getMetaInfo(id);
     title.value = res.title ?? "Unknown";
     icon.value = res.icon ?? {
       type: "default",
@@ -65,9 +67,10 @@ watchEffect(async () => {
   }
 });
 
-const openPage = () => {
+const openPage = async () => {
   const targetId = targetPageId.value;
   if (targetId) {
+    await globalNavigationStore.openPage(targetId);
   }
 };
 </script>
